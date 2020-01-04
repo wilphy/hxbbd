56b9cd9ef9073140bb5065f443889fece3100d52
wey <149201216@qq.com>
2020-01-04 16:50:16 +0800

    实现模糊搜索

--- cloudfunctions/blog/index.js
+++ cloudfunctions/blog/index.js
@@ -10,13 +10,25 @@ const db = cloud.database()
 const blogCollection = db.collection('blog')
 
 // 云函数入口函数
-exports.main = async(event, context) => {
+exports.main = async (event, context) => {
   const app = new TcbRouter({
     event
   })
 
-  app.router('list', async(ctx, next) => {
+  app.router('list', async (ctx, next) => {
+    const keyword = event.keyword
+    let w = {}
+    if (keyword.trim() !== '') {
+      w = {
+        content: new db.RegExp({
+          regexp: keyword,
+          options: 'i' //忽略大小写
+        })
+      }
+    }
+
     let blogList = await blogCollection
+      .where(w)
       .skip(event.start)
       .limit(event.count)
       .orderBy('createTime', 'desc')
--- miniprogram/components/search/search.js
+++ miniprogram/components/search/search.js
@@ -1,4 +1,7 @@
 // components/search/search.js
+
+let keyword = '' //搜索关键字
+
 Component({
   /**
    * 组件的属性列表
@@ -21,6 +24,15 @@ Component({
    * 组件的方法列表
    */
   methods: {
+    onInput(event) {
+      keyword = event.detail.value
+    },
 
+    onSearch() {
+      // console.log(keyword)
+      this.triggerEvent('search', {
+        keyword
+      })
+    }
   }
 })
